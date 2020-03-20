3143b1b8db7d4211c5018729bc2071690c57444e
Роман Егоров <r.egorov@qsoft.ru>
2020-03-20 18:00:21 +0300

    131080 / create renessans in progress

--- app/Services/Company/Renessans/RenessansCreateService.php
+++ app/Services/Company/Renessans/RenessansCreateService.php
@@ -11,7 +11,6 @@ class RenessansCreateService extends RenessansService implements RenessansCreate
 {
     protected $apiPath = [
         'sendCreate' => '/create/',
-        //'receiveCalculate' => '/policy/:policyId/status/',
     ];
 
     private $catalogPurpose = ["Личная", "Такси"]; // TODO: значение из справочника, справочник нужно прогружать при валидации, будет кэшироваться
@@ -52,354 +51,35 @@ class RenessansCreateService extends RenessansService implements RenessansCreate
         return $response['data'];
     }
 
-    public function map(): array
+    protected function prepareData($attributes): array
     {
-        return [
-            'purpose' => [
-                'required' => true,
-                'type' => 'string',
-                'in' => $this->catalogPurpose,
-            ],
-            'isInsurerJuridical' => [
-                'required' => false,
-                'type' => 'boolean',
-                'default' => 0,
-            ],
+        $insurer = $this->searchSubjectById($attributes, $attributes['policy']['insurantId']);
+        $owner = $this->searchSubjectById($attributes, $attributes['policy']['ownerId']);
+        $data = [
+            'calculationId' => $attributes['calculationId'],
+            'purpose' => $attributes['car']['vehicleUsage'],
             'cabinet' => [
-                'required' => true,
-                'type' => 'object',
-                'array' => [
-                    'email' => [
-                        'required' => true,
-                        'type' => 'email',
-                    ],
-                ],
+                'email' => $insurer['email'],
             ],
-            'owner' => [
-                'required' => true,
-                'type' => 'object',
-                'array' => [
-                    'email' => [
-                        'required' => true,
-                        'type' => 'email',
-                    ],
-                    'phone' => [
-                        'required' => true,
-                        'type' => 'integer',
-                    ],
-                    'addressJuridical' => [
-                        'required' => true,
-                        'type' => 'object',
-                        'array' => [
-                            'country' => [
-                                'required' => true,
-                                'type' => 'string',
-                            ],
-                            'zip' => [
-                                'required' => true,
-                                'type' => 'integer',
-                            ],
-                            'city' => [
-                                'required' => false,
-                                'type' => 'string',
-                            ],
-                            'settlement' => [
-                                'required_without' => 'owner.addressJuridical.city',
-                                'type' => 'string',
-                            ],
-                            'street' => [
-                                'required' => true,
-                                'type' => 'string',
-                            ],
-                            'home' => [
-                                'required' => true,
-                                'type' => 'string',
-                            ],
-                            'flat' => [
-                                'required' => true,
-                                'type' => 'string',
-                            ],
-                            'kladr' => [
-                                'required' => true,
-                                'type' => 'integer',
-                            ],
-                        ],
-                    ],
-                    'addressFact' => [
-                        'required' => true,
-                        'type' => 'object',
-                        'array' => [
-                            'country' => [
-                                'required' => true,
-                                'type' => 'string',
-                            ],
-                            'zip' => [
-                                'required' => true,
-                                'type' => 'integer',
-                            ],
-                            'city' => [
-                                'required' => false,
-                                'type' => 'string',
-                            ],
-                            'settlement' => [
-                                'required_without' => 'owner.addressFact.city',
-                                'type' => 'string',
-                            ],
-                            'street' => [
-                                'required' => true,
-                                'type' => 'string',
-                            ],
-                            'home' => [
-                                'required' => true,
-                                'type' => 'string',
-                            ],
-                            'flat' => [
-                                'required' => true,
-                                'type' => 'string',
-                            ],
-                            'kladr' => [
-                                'required' => true,
-                                'type' => 'integer',
-                            ],
-                        ],
-                    ],
-                    'document' => [
-                        'required' => false,
-                        'type' => 'object',
-                        'array' => [
-                            'typeofdocument' => [
-                                'required' => false,
-                                'type' => 'string',
-                                'in' => $this->catalogTypeOfDocument,
-                            ],
-                            'dateIssue' => [
-                                'required' => true,
-                                'type' => 'date',
-                                'date_format' => 'Y-m-d',
-                            ],
-                            'issued' => [
-                                'required' => true,
-                                'type' => 'string',
-                            ],
-                            'number' => [
-                                'required' => true,
-                                'type' => 'string',
-                            ],
-                            'series' => [
-                                'required' => true,
-                                'type' => 'string',
-                            ],
-                            'codeDivision' => [
-                                'required' => false,
-                                'type' => 'integer',
-                            ],
-                        ],
-                    ],
-                ],
-            ],
-            'insurer' => [
-                'required' => true,
-                'type' => 'object',
-                'array' => [
-                    'name' => [
-                        'required' => true,
-                        'type' => 'string',
-                    ],
-                    'lastname' => [
-                        'required' => true,
-                        'type' => 'string',
-                    ],
-                    'middlename' => [
-                        'required' => false,
-                        'type' => 'string',
-                    ],
-                    'birthday' => [
-                        'required' => true,
-                        'type' => 'date',
-                        'date_format' => 'Y-m-d',
-                    ],
-                    'email' => [
-                        'required' => true,
-                        'type' => 'email',
-                    ],
-                    'phone' => [
-                        'required' => true,
-                        'type' => 'integer',
-                    ],
-                    'addressJuridical' => [
-                        'required' => true,
-                        'type' => 'object',
-                        'array' => [
-                            'country' => [
-                                'required' => true,
-                                'type' => 'string',
-                            ],
-                            'zip' => [
-                                'required' => true,
-                                'type' => 'integer',
-                            ],
-                            'city' => [
-                                'required' => false,
-                                'type' => 'string',
-                            ],
-                            'settlement' => [
-                                'required_without' => 'insurer.addressJuridical.city',
-                                'type' => 'string',
-                            ],
-                            'street' => [
-                                'required' => true,
-                                'type' => 'string',
-                            ],
-                            'home' => [
-                                'required' => true,
-                                'type' => 'string',
-                            ],
-                            'flat' => [
-                                'required' => true,
-                                'type' => 'string',
-                            ],
-                            'kladr' => [
-                                'required' => true,
-                                'type' => 'integer',
-                            ],
-                        ],
-                    ],
-                    'addressFact' => [
-                        'required' => true,
-                        'type' => 'object',
-                        'array' => [
-                            'country' => [
-                                'required' => true,
-                                'type' => 'string',
-                            ],
-                            'zip' => [
-                                'required' => true,
-                                'type' => 'integer',
-                            ],
-                            'city' => [
-                                'required' => false,
-                                'type' => 'string',
-                            ],
-                            'settlement' => [
-                                'required_without' => 'insurer.addressFact.city',
-                                'type' => 'string',
-                            ],
-                            'street' => [
-                                'required' => true,
-                                'type' => 'string',
-                            ],
-                            'home' => [
-                                'required' => true,
-                                'type' => 'string',
-                            ],
-                            'flat' => [
-                                'required' => true,
-                                'type' => 'string',
-                            ],
-                            'kladr' => [
-                                'required' => true,
-                                'type' => 'integer',
-                            ],
-                        ],
-                    ],
-                    'document' => [
-                        'required' => true,
-                        'type' => 'object',
-                        'array' => [
-                            'typeofdocument' => [
-                                'required' => false,
-                                'type' => 'string',
-                                'in' => $this->catalogTypeOfDocument,
-                            ],
-                            'dateIssue' => [
-                                'required' => true,
-                                'type' => 'date',
-                                'date_format' => 'Y-m-d',
-                            ],
-                            'issued' => [
-                                'required' => true,
-                                'type' => 'string',
-                            ],
-                            'number' => [
-                                'required' => true,
-                                'type' => 'string',
-                            ],
-                            'series' => [
-                                'required' => true,
-                                'type' => 'string',
-                            ],
-                        ],
-                    ],
-                ],
-            ],
-            'car' => [
-                'required' => true,
-                'type' => 'object',
-                'array' => [
-                    'year' => [
-                        'required' => true,
-                        'type' => 'integer',
-                    ],
-                ],
-                'pts' => [
-                    'required_without' => 'car.sts',
-                    'type' => 'object',
-                    'array' => [
-                        'dateIssue' => [
-                            'required' => true,
-                            'type' => 'date',
-                            'date_format' => 'Y-m-d',
-                        ],
-                        'number' => [
-                            'required' => true,
-                            'type' => 'string',
-                        ],
-                        'serie' => [
-                            'required' => true,
-                            'type' => 'string',
-                        ],
-                    ],
-                ],
-                'sts' => [
-                    'required_without' => 'car.pts',
-                    'type' => 'object',
-                    'array' => [
-                        'dateIssue' => [
-                            'required' => true,
-                            'type' => 'date',
-                            'date_format' => 'Y-m-d',
-                        ],
-                        'number' => [
-                            'required' => true,
-                            'type' => 'string',
-                        ],
-                        'serie' => [
-                            'required' => true,
-                            'type' => 'string',
-                        ],
-                        'docType' => [
-                            'required' => true,
-                            'type' => 'string',
-                            'in' => $this->catalogStsDocType,
-                        ],
-                    ],
-                ],
-            ],
-            'diagnostic' => [
-                'required' => false,
-                'type' => 'object',
-                'array' => [
-                    'number' => [
-                        'required' => true,
-                        'type' => 'string',
-                    ],
-                    'validDate' => [
-                        'required' => true,
-                        'type' => 'date',
-                        'date_format' => 'Y-m-d',
-                    ],
-                ],
-            ],
+            'isInsurerJuridical' => $this->transformBooleanToInteger(false),
+        ];
+        $data['insurer'] = $this->getSubjectData($insurer);
+        return $data;
+    }
+
+    protected function getSubjectData($subject)
+    {
+        $subjectData = [
+            'email' => $subject['email'],
+            'phone' => $subject['phone'],
+        ];
+        $regAddress = $this->searchAddressByType($subject, 'registration'); //todo справочник
+    }
+
+    protected function getAddressData($address)
+    {
+        $addressData = [
+
         ];
     }
 
