5e04c2b2daddb0210960db2fabbe84799dae30b1
Василий Метт <v.mett@qsoft.ru>
2020-03-26 13:45:09 +0300

    132296/Подключение пакета авторизации

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -91,7 +91,8 @@ $app->configure('api_sk');
 */
 
 $app->register(App\Providers\AppServiceProvider::class);
-//$app->register(Avtocod\B2BApi\Laravel\ServiceProvider::class);
+$app->register(\Benfin\Api\BenfinMicroserviceProvider::class);
+$app->routeMiddleware([ 'auth' => Benfin\Auth\Http\Middleware\Authenticate::class, ]);
 // $app->register(App\Providers\AuthServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
 
