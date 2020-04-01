9d87bc088db761726d20a71f9d186298d1ebf242
Roman <r.egorov@qsoft.ru>
2020-04-01 17:41:41 +0300

    132799 / add cache config

--- config/cache.php
+++ config/cache.php
@@ -1,4 +1,23 @@
 <?php
 return [
     'guidesCacheTtl' => env('GUIDES_CACHE_TTL'),
+    'redis' => [
+
+        'client' => 'predis',
+
+        'default' => [
+            'host' => env('REDIS_HOST', '127.0.0.1'),
+            'password' => env('REDIS_PASSWORD', null),
+            'port' => env('REDIS_PORT', 6379),
+            'database' => env('REDIS_DB', 0),
+        ],
+
+        'cache' => [
+            'host' => env('REDIS_HOST', '127.0.0.1'),
+            'password' => env('REDIS_PASSWORD', null),
+            'port' => env('REDIS_PORT', 6379),
+            'database' => env('REDIS_CACHE_DB', 1),
+        ],
+
+    ],
 ];
