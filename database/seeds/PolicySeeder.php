77b74ca260403775a5eb73b98759c2f6ca7136e1
a.vasilev <a.vasilev@qsoft.ru>
2020-04-17 14:37:43 +0300

    133546 / Добавил файлы логов

--- database/seeds/PolicySeeder.php
+++ database/seeds/PolicySeeder.php
@@ -6,10 +6,28 @@ class PolicySeeder extends Seeder
 {
     protected static $files = [
         [
-            'name' => 1,
-            'dir' => 1,
-            'content_type' => 1,
-            'size' => 1,
+            'name' => 'renessans.svg',
+            'dir' => 'http://172.27.1.121:9000/car-insurance/insurance-companies_logo/renessans.svg',
+            'content_type' => 'image/svg+xml',
+            'size' => 1619,
+        ],
+        [
+            'name' => 'ingosstrah.svg',
+            'dir' => 'http://172.27.1.121:9000/car-insurance/insurance-companies_logo/ingosstrah.svg',
+            'content_type' => 'image/svg+xml',
+            'size' => 358,
+        ],
+        [
+            'name' => 'soglasie.svg',
+            'dir' => 'http://172.27.1.121:9000/car-insurance/insurance-companies_logo/soglasie.svg',
+            'content_type' => 'image/svg+xml',
+            'size' => 1348,
+        ],
+        [
+            'name' => 'tinkoff.svg',
+            'dir' => 'http://172.27.1.121:9000/car-insurance/insurance-companies_logo/tinkoff.svg',
+            'content_type' => 'image/svg+xml',
+            'size' => 3728,
         ],
     ];
 
@@ -23,19 +41,19 @@ class PolicySeeder extends Seeder
         ],
         [
             'active' => true,
-            'logo_id' => 1,
+            'logo_id' => 2,
             'code' => 'ingosstrah',
             'name' => 'Ингосстрах',
         ],
         [
             'active' => true,
-            'logo_id' => 1,
+            'logo_id' => 3,
             'code' => 'soglasie',
             'name' => 'Согласие',
         ],
         [
             'active' => true,
-            'logo_id' => 1,
+            'logo_id' => 4,
             'code' => 'tinkoff',
             'name' => 'Тинькофф',
         ],
