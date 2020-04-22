fca4743b9e23b5b3a0fce6570541445e45011460
Sergey Yefremov <s.efremov@qsoft.ru>
2020-04-22 10:34:52 +0300

    130726 / Поправил баги при изменении черновика

--- app/Traits/ValueSetterTrait.php
+++ app/Traits/ValueSetterTrait.php
@@ -9,7 +9,10 @@ trait ValueSetterTrait
     public function setValuesByArray(&$target, $dependencies, $source)
     {
         foreach ($dependencies as $targetName => $sourceName) {
-            if (isset($source[$sourceName]) && $source[$sourceName]) {
+            if (
+                isset($source[$sourceName]) && $source[$sourceName] ||
+                isset($source[$sourceName]) && gettype($source[$sourceName]) == 'boolean'
+            ) {
                 if (gettype($source[$sourceName]) == 'array') {
                     continue;
                 }
