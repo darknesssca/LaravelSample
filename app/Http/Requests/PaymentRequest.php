8de90e29fad97e5cccd802822a907d0c8b686824
Roman <r.egorov@qsoft.ru>
2020-05-07 06:39:22 -0700

    135565 / remake request for tinkoff  set update status

--- app/Http/Requests/PaymentRequest.php
+++ app/Http/Requests/PaymentRequest.php
@@ -10,7 +10,7 @@ class PaymentRequest extends AbstractRequest
     public function rules():array
     {
         return [
-            // в связи с тем, что на вход подается распарсенная произвольная xml, валидация в данном случае не нужна
+            'policyNumber' => 'required|string',
         ];
     }
 }
