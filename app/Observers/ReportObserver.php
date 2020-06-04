411784362f61a9d63bf910158374a842c71a78c8
Sergey Efremov <s.efremov@qsoft.ru>
2020-06-04 15:48:17 +0300

    137879 / Сброс кеша при создании отчета, отлов исключений guzzle

--- app/Observers/ReportObserver.php
+++ app/Observers/ReportObserver.php
@@ -5,7 +5,10 @@ namespace App\Observers;
 
 
 use App\Cache\ReportCacheTag;
+use App\Models\Report;
+use Benfin\Api\GlobalStorage;
 use Benfin\Cache\CacheKeysTrait;
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Cache;
 
 trait ReportObserver
@@ -15,10 +18,13 @@ trait ReportObserver
     protected static function boot()
     {
         parent::boot();
-
+        static::creating(function ($model) {
+            $model->create_date = Carbon::now();
+            $model->creator_id = GlobalStorage::getUserId();
+            $model->is_payed = false;
+        });
         static::created(function ($model) {
             Cache::tags(self::getReportCacheTagByAttribute('Filter'))->flush();
-
             $cacheTag = $model->creator_id;
             Cache::tags(self::getReportCacheTagByAttribute("Creator|$cacheTag"))->flush();
         });
