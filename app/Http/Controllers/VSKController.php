4853f9e58b8dd6824174231d8e4e81039e5f5a54
a.vasilev <a.vasilev@qsoft.ru>
2020-09-17 14:01:58 +0300

    144811 / Поправил ошибку

--- app/Http/Controllers/VSKController.php
+++ app/Http/Controllers/VSKController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 
 
 use App\Contracts\Company\Vsk\VskCallbackServiceContract;
+use App\Contracts\Repositories\Services\InsuranceCompanyServiceContract;
 use App\Http\Requests\VSK\CallbackRequest;
 use App\Http\Requests\VSK\SignRequest;
 use App\Traits\CompanyServicesTrait;
@@ -15,10 +16,14 @@ class VSKController extends Controller
     use CompanyServicesTrait;
 
     private $callbackService;
+    private $insuranceCompanyService;
 
-    public function __construct(VskCallbackServiceContract $callbackService)
-    {
+    public function __construct(
+        VskCallbackServiceContract $callbackService,
+        InsuranceCompanyServiceContract $insuranceCompanyService
+    ) {
         $this->callbackService = $callbackService;
+        $this->insuranceCompanyService = $insuranceCompanyService;
     }
 
     public function callback(CallbackRequest $request)
@@ -30,7 +35,7 @@ class VSKController extends Controller
 
     public function sign(SignRequest $request)
     {
-        $fields = $request->validate();
+        $fields = $request->validated();
         $company = $this->getCompany('vsk');
         $this->runService($company, $fields, 'creating');
         return Response::success(['status' => 'processing']);
