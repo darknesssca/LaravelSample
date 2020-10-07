c6691835c66130e6b477c5de125b5491d0851f9f
a.vasilev <a.vasilev@qsoft.ru>
2020-10-07 17:42:12 +0300

    150230 / Поправил ошибку в киви

--- app/Repositories/ReportRepository.php
+++ app/Repositories/ReportRepository.php
@@ -92,7 +92,7 @@ class ReportRepository implements ReportRepositoryContract
             // Получение всех записей из БД
             $count = !empty($filter['count']) ? $filter['count'] : 10;
             if (isset($filter['count']) && $filter['count'] === false) {
-                $count = null;
+                $count = PHP_INT_MAX;
             }
             $page = !empty($filter['page']) ? $filter['page'] : 1;
             return $query->paginate(
