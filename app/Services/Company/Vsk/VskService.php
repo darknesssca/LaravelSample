62d96c4d6a36a45ec1af2af865eaa8a773c75084
a.vasilev <a.vasilev@qsoft.ru>
2020-11-03 10:28:32 +0300

    146894 / Поправил ошибку несоответствия названий ТС для РСА

--- app/Services/Company/Vsk/VskService.php
+++ app/Services/Company/Vsk/VskService.php
@@ -165,12 +165,12 @@ abstract class VskService extends CompanyService
                 ],
                 'model:model' => [
                     'model:mark' => [
-                        'model:name' => $attributes['car']['maker_name']
+                        'model:name' => ucfirst($attributes['car']['maker_name'])
                     ],
                     'model:type' => [
                         'model:vehicleTypeCode' => $category['name']
                     ],
-                    'model:name' => $attributes['car']['model']
+                    'model:name' => ucfirst($attributes['car']['model'])
                 ],
                 'model:vin' => $attributes['car']['vin'],
                 'model:bodyNumber' => '',
